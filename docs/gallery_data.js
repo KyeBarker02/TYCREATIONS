ffcea01ce3b86111d6ae7eb908b465a7fbf2e8f3
KJB B <khercb@gmail.com>
2026-06-24 19:24:33 +0100

    updated gallery previews

--- docs/gallery_data.js
+++ docs/gallery_data.js
@@ -9,21 +9,21 @@ function generateImages(folder, prefix, count, ext = 'jpeg') {
 window.galleryData = {
     HoneyCakes: generateImages('HoneyCakes', 'HoneyCake', 33),
     HoneyCakesPreview: [
-        '/images/HoneyCakes/HoneyCake16.jpeg',
-        '/images/HoneyCakes/HoneyCake61.jpeg',
-        '/images/HoneyCakes/HoneyCake66.jpeg',
-        '/images/HoneyCakes/HoneyCake23.jpeg',
+        '/images/HoneyCakes/HoneyCake29.jpeg',
+        '/images/HoneyCakes/HoneyCake30.jpeg',
+        '/images/HoneyCakes/HoneyCake31.jpeg',
+        '/images/HoneyCakes/HoneyCake15.jpeg',
     ],
 
     BirthdayCakes: generateImages('BirthdayCakes', 'BirthdayCake', 30),
     BirthdayCakesPreview: [
+        '/images/BirthdayCakes/BirthdayCake2.jpeg',
+        '/images/BirthdayCakes/BirthdayCake10.jpeg',
+        '/images/BirthdayCakes/BirthdayCake15.jpeg',
         '/images/BirthdayCakes/BirthdayCake1.jpeg',
-        '/images/BirthdayCakes/BirthdayCake60.jpeg',
-        '/images/BirthdayCakes/BirthdayCake33.jpeg',
-        '/images/BirthdayCakes/BirthdayCake6.jpeg',
-        '/images/BirthdayCakes/BirthdayCake21.jpeg',
+        '/images/BirthdayCakes/BirthdayCake28.jpeg',
+        '/images/BirthdayCakes/BirthdayCake29.jpeg',
         '/images/BirthdayCakes/BirthdayCake30.jpeg',
-        '/images/BirthdayCakes/BirthdayCake16.jpeg',
     ],
 
     WeddingCakes: generateImages('WeddingCakes', 'WeddingCake', 6),
@@ -39,17 +39,17 @@ window.galleryData = {
         '/images/OtherCakes/OtherCake14.jpeg',
         '/images/OtherCakes/OtherCake27.jpeg',
         '/images/OtherCakes/OtherCake34.jpeg',
-        '/images/OtherCakes/OtherCake41.jpeg',
-        '/images/OtherCakes/OtherCake56.jpeg',
-        '/images/OtherCakes/OtherCake39.jpeg',
+        '/images/OtherCakes/OtherCake10.jpeg',
+        '/images/OtherCakes/OtherCake18.jpeg',
+        '/images/OtherCakes/OtherCake23.jpeg',
         
     ],
 
     Cookies: generateImages('Cookies', 'Cookie', 8),
     CookiesPreview: [
         '/images/Cookies/Cookie1.jpeg',
+        '/images/Cookies/Cookie3.jpeg',
         '/images/Cookies/Cookie8.jpeg',
-        '/images/Cookies/Cookie9.jpeg',
         '/images/Cookies/Cookie3.jpeg',
         '/images/Cookies/Cookie7.jpeg',
     ],
@@ -61,9 +61,9 @@ window.galleryData = {
         '/images/Desserts/Desserts7.jpeg',
         '/images/Desserts/Desserts9.jpeg',
         '/images/Desserts/Desserts11.jpeg',
-        '/images/Desserts/Desserts22.jpeg',
-        '/images/Desserts/Desserts24.jpeg',
-        '/images/Desserts/Desserts23.jpeg',
+        '/images/Desserts/Desserts17.jpeg',
+        '/images/Desserts/Desserts14.jpeg',
+        '/images/Desserts/Desserts1.jpeg',
     ]
 };
 
